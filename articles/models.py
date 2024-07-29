c69b1d51cb450148563a92bdfb5158a9bc92cf93
Rishat Nuriev <rnuriev@dealersocket.com>
2024-07-29 22:56:51 +0200

    customize django taggit slugify method

--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
 from taggit.managers import TaggableManager
+from taggit.models import TagBase
 
 from tournaments.models import Tournament
+
+def translit_to_eng(s: str) -> str:
+    d = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+         'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'к': 'k',
+         'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
+         'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch',
+         'ш': 'sh', 'щ': 'shch', 'ь': '', 'ы': 'y', 'ъ': '', 'э': 'r', 'ю': 'yu', 'я': 'ya'}
+
+    return "".join(map(lambda x: d[x] if d.get(x, False) else x, s.lower()))
+
+
+class TranslitTag(TagBase):
+    def slugify(self, tag, i=None):
+        slug = super().slugify(tag, i)
+        return slugify(translit_to_eng(slug))
+
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -11,22 +29,36 @@ class PublishedManager(models.Manager):
 
 class Article(models.Model):
 
-    tags = TaggableManager()
+    tags = TaggableManager(through=TranslitTag)
 
     class Status(models.IntegerChoices):
-        DRAFT = 0, 'Черновик'
-        PUBLISHED = 1, 'Опубликовано'
+        DRAFT = 0, "Черновик"
+        PUBLISHED = 1, "Опубликовано"
 
     title = models.CharField(max_length=255, verbose_name="Заголовок")
-    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="Slug")
+    slug = models.SlugField(
+        max_length=255, unique=True, db_index=True, verbose_name="Slug"
+    )
     content = models.TextField(blank=True, verbose_name="Генерируемый текст статьи")
-    additional_content = models.TextField(blank=True, null=True, verbose_name="Дополнительный текст")
+    additional_content = models.TextField(
+        blank=True, null=True, verbose_name="Дополнительный текст"
+    )
 
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
-    is_published = models.BooleanField(choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
-                                       default=Status.DRAFT, verbose_name="Статус")
-    tournament = models.ForeignKey(Tournament, on_delete=models.PROTECT, related_name='posts', verbose_name="Турнир", null=True, blank=True)
+    is_published = models.BooleanField(
+        choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
+        default=Status.DRAFT,
+        verbose_name="Статус",
+    )
+    tournament = models.ForeignKey(
+        Tournament,
+        on_delete=models.PROTECT,
+        related_name="posts",
+        verbose_name="Турнир",
+        null=True,
+        blank=True,
+    )
     match_day = models.DateField(verbose_name="Игровой день", blank=True, null=True)
 
     objects = models.Manager()
@@ -38,10 +70,8 @@ class Article(models.Model):
     class Meta:
         verbose_name = "Статья"
         verbose_name_plural = "Статьи"
-        ordering = ['-time_create']
-        indexes = [
-            models.Index(fields=['-time_create'])
-        ]
+        ordering = ["-time_create"]
+        indexes = [models.Index(fields=["-time_create"])]
 
     def get_absolute_url(self):
-        return reverse('article', kwargs={'article_slug': self.slug})
+        return reverse("article", kwargs={"article_slug": self.slug})
