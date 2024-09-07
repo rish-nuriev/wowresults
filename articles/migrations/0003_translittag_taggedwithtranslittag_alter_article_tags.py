a5ad2d4f353b64ec7080ad414e8d39c34962c15a
Rishat Nuriev <rnuriev@dealersocket.com>
2024-09-07 23:30:31 +0200

    correct existing migration for tags

--- articles/migrations/0003_translittag_taggedwithtranslittag_alter_article_tags.py
+++ articles/migrations/0003_translittag_taggedwithtranslittag_alter_article_tags.py
@@ -7,10 +7,6 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('contenttypes', '0002_remove_content_type_name'),
-    ]
-
     operations = [
         migrations.CreateModel(
             name='TranslitTag',
