a918862cf4db63a55fe630369b3d9eb180adae04
Rishat Nuriev <rnuriev@dealersocket.com>
2024-09-10 15:06:07 +0200

    add similar articles

---
+++ articles/templatetags/article_tags.py
@@ -0,0 +1,18 @@
+from django import template
+from django.db.models import Count
+from articles.models import Article
+
+register = template.Library()
+
+
+@register.inclusion_tag("articles/similar.html")
+def show_similar_articles(article):
+    # Список схожих постов
+    articles_tags_ids = article.tags.values_list("id", flat=True)
+    similar_articles = Article.published.filter(tags__in=articles_tags_ids).exclude(
+        id=article.id
+    )
+    similar_articles = similar_articles.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-time_create"
+    )[:4]
+    return {"similar_articles": similar_articles}
