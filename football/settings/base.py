395ad2c0e323218ac0f27b42acfa92a8f1344ab5
rish-nuriev <nuriev.rish@gmail.com>
2025-01-10 23:22:13 +0200

    set proper configuration for debug parameter

--- football/settings/base.py
+++ football/settings/base.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = config("DEBUG") == 'True'
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "rish333.pythonanywhere.com"]
 
