882689c24fd2b8c0d40c0546ca224059e25c0a5d
rish-nuriev <nuriev.rish@gmail.com>
2025-02-12 21:25:59 +0200

    Add registration

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from account import views
 
 urlpatterns = [
-    path("", include('django.contrib.auth.urls')),
-    path("", views.dashboard, name="dashboard"),
+    path('', include('django.contrib.auth.urls')),
+    path('', views.dashboard, name="dashboard"),
+    path('register/', views.register, name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 def user_login(request):
@@ -24,6 +24,23 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, "account/login.html", {"form": form})
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Создать новый объект пользователя,
+            # но пока не сохранять его
+            new_user = user_form.save(commit=False)
+            # Установить выбранный пароль
+            new_user.set_password(user_form.cleaned_data["password"])
+            # Сохранить объект User
+            new_user.save()
+            return render(request, "account/register_done.html", {"new_user": new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, "account/register.html", {"user_form": user_form})
 
 
 @login_required
