c88f9f958274eac8b04ac8cea9fa6b0653e32527
rish-nuriev <nuriev.rish@gmail.com>
2025-02-17 22:24:31 +0200

    beautify login and forgot password forms

--- account/forms.py
+++ account/forms.py
@@ -1,22 +1,29 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+class PrettyAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
+class PrettyPasswordResetForm(PasswordResetForm):
+    email = forms.EmailField(
+        label='Email',
+        max_length=254,
+        widget=forms.EmailInput(attrs={'autocomplete': 'email', 'class': 'form-control'}),
+    )
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Повторите пароль", widget=forms.PasswordInput)
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ["username", "first_name", "email"]
+        fields = ['username', 'first_name', 'email']
 
     def clean_password2(self):
         cd = self.cleaned_data
-        if cd["password"] != cd["password2"]:
-            raise forms.ValidationError("Пароли не совпадают")
-        return cd["password2"]
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Пароли не совпадают')
+        return cd['password2']
--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,18 @@
 from django.urls import include, path
+from django.contrib.auth.views import LoginView, PasswordResetView
 from account import views
+from account.forms import PrettyAuthenticationForm, PrettyPasswordResetForm
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
-    path('', views.dashboard, name="dashboard"),
-    path('register/', views.register, name='register'),
+    path(
+        "login/", LoginView.as_view(form_class=PrettyAuthenticationForm), name="login"
+    ),
+    path(
+        "password_reset/",
+        PasswordResetView.as_view(form_class=PrettyPasswordResetForm),
+        name="password_reset",
+    ),
+    path("", include("django.contrib.auth.urls")),
+    path("", views.dashboard, name="dashboard"),
+    path("register/", views.register, name="register"),
 ]
