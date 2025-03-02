e6cbbbc1dd663fc1bb7ae5aa1759da683cfb970e
rish-nuriev <nuriev.rish@gmail.com>
2025-03-02 19:30:18 +0200

    better fix of check_celery_connection

--- football/utils/celery_utils.py
+++ football/utils/celery_utils.py
@@ -1,6 +1,7 @@
 import logging
-from celery.exceptions import CeleryError
+from celery.app.control import Inspect
 from football.celery import app
+
 
 logger = logging.getLogger("basic_logger")
 
@@ -8,15 +9,19 @@ logger = logging.getLogger("basic_logger")
 def check_celery_connection():
     """
     Функция проверяет запущен ли celery.
-    Нужна для того чтобы выполнить задачу (например отправить письмо) 
+    Нужна для того чтобы выполнить задачу (например отправить письмо)
     обычным способом в случае если celery недоступен.
     """
     try:
-        control = app.control
-        ping_response = control.ping(timeout=1)
-        if not ping_response:
-            raise CeleryError("No response from Celery workers")
-        return True
-    except CeleryError as e:
+        inspector = Inspect(app=app)
+        active_tasks = inspector.active()
+        if active_tasks:
+            # есть активные воркеры celery
+            return True
+        else:
+            # активных воркеров celery нет
+            return False
+    except Exception as e:
+        # логируем неизвестную ошибку
         logger.error(e)
         return False
