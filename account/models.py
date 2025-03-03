6584b130a2dcd76323c004ea1b069724a7391775
rish-nuriev <nuriev.rish@gmail.com>
2025-03-03 18:16:07 +0200

    add ability to adjust profile

--- account/models.py
+++ account/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to="users/%Y/%m/%d/", blank=True)
+
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
+
+    def __str__(self):
+        return f"Профиль {self.user.username}"
--- account/views.py
+++ account/views.py
@@ -5,7 +5,8 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.urls import reverse
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
 
 
 def register(request):
@@ -21,13 +22,17 @@ def register(request):
             user = User.objects.create_user(
                 username=user_username, email=user_email, password=user_password
             )
+            Profile.objects.create(user=user)
 
             # Сделать пользователя неверифицированным
             # пока не пройдет проверку по Email
             user.is_active = False
             send_email(user)
-            messages.success(request, f'Письмо с инструкцией по активации \
-                                        отправлено на {user_email}')
+            messages.success(
+                request,
+                f"Письмо с инструкцией по активации \
+                                        отправлено на {user_email}",
+            )
 
             return HttpResponseRedirect(reverse("login"))
 
@@ -39,3 +44,26 @@ def register(request):
 @login_required
 def dashboard(request):
     return render(request, "account/dashboard.html", {"section": "dashboard"})
+
+
+@login_required
+def edit(request):
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile, data=request.POST, files=request.FILES
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Профиль успешно обновлен')
+        else:
+            messages.error(request, 'Ошибка редактирования профиля')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(
+        request,
+        "account/edit.html",
+        {"user_form": user_form, "profile_form": profile_form},
+    )
