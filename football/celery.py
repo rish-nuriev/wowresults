14d6561296623ba3a46eed12c678e4aefe9b05a9
rish-nuriev <rishat.nuriev@solera.com>
2025-08-21 17:59:39 +0500

    load all the environment variables from .env file

--- football/celery.py
+++ football/celery.py
@@ -1,9 +1,20 @@
 import os
-from celery import Celery # type: ignore
+from celery import Celery
+from pathlib import Path
+from dotenv import load_dotenv
 
-# задать стандартный модуль настроек Django
-# для программы 'celery'.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "football.settings")
+# Загружаем .env так же, как в manage.py
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
+
+# Устанавливаем настройки Django (по умолчанию local)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "football.settings.local")
+
 app = Celery("football")
+
+# Подгружаем настройки Django с префиксом CELERY_
+# Например, CELERY_BROKER_URL, CELERY_RESULT_BACKEND
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Автоматически искать tasks.py во всех приложениях
 app.autodiscover_tasks()
